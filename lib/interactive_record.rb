635d7825ac34d2c26140a4f48271a1e4b51216cf
Charles Butler <cdbutle@outlook.com>
2020-04-17 22:42:50 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,36 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def self.table_name
+
+  end
+
+  def self.column_names
+
+  end
+
+
+#Instance methods to insert data into db
+
+  def table_name_for_insert
+
+  end
+
+  def col_names_for_insert
+  end
+
+  def save
+  end
+
+  #More class methods
+
+  def self.find_by_name
+
+  end
+
+  def self.find_by_name
+
+  end
   
 end
