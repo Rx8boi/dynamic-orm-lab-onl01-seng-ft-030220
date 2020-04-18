e9152cd256ca21887291a71071f8baaff0be6c76
Charles Butler <cdbutle@outlook.com>
2020-04-18 00:03:18 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ class InteractiveRecord
 
   def save
     DB[:conn].execute("INSERT INTO #{table_name_for_insert}
-     (#{col_names_for_insert}) VALUES (?), [values_for_insert]")
+     (#{col_names_for_insert}) VALUES (?)," [values_for_insert, value])
 
     @id = DB[:conn].execute("SELECT last_insert_rowid()
      FROM #{table_name_for_insert}")[0][0]
@@ -59,7 +59,7 @@ class InteractiveRecord
   #More class methods
 
   def self.find_by_name
-    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?, [name]")
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?," [name, value])
   end
 
   def self.find_by
